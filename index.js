070ffbff32c971371ea63bd731c98a3dc651a228
Frederik Sicking <frederik@dersicking.de>
2023-03-24 16:12:03 +0100

    landmark detection now on GPU (much faster) and basic hand gesture recognition is working

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ let initDone;
 let videoDone;
 
 async function init() {
-	poseDetector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, { modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING });
+	poseDetector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER });
 
 	const model = handPoseDetection.SupportedModels.MediaPipeHands;
 	const detectorConfig = {
-		runtime: 'tfjs',
+		runtime: 'mediapipe', // or 'tfjs',
+		solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands',
 		modelType: 'full'
-	};
+	}
 	handDetector = await handPoseDetection.createDetector(model, detectorConfig);
 
 	initDone = true;
@@ -30,6 +31,11 @@ async function setup() {
 	createCanvas(width, height);
 	video = createCapture(VIDEO, () => { videoDone = true; frameSize = Math.min(video.width, video.height) });
 	video.hide();
+
+	gestureInput = createInput("", "text");
+	createButton("Store Gesture").mouseClicked(() => {
+		storeGesture(gestureInput.elt.value, leftHand.hand);
+	});
 
 	await init();
 }
@@ -53,7 +59,7 @@ const fillColors = [
 	[255, 0, 0],
 	[255, 0, 0],
 	[255, 0, 0],
-	[0, 0, 255],
+	[255, 0, 255],
 	[0, 0, 255],
 	[255, 0, 0],
 	[255, 0, 0],
@@ -76,10 +82,55 @@ function drawPoseKeypoints() {
 	}
 }
 
+let leftHand = {}, rightHand = {};
+
+function pickHands() {
+	if (!poses || poses.length < 1) return;
+
+	let leftWrist = poses[0].keypoints[9];
+	let rightWrist = poses[0].keypoints[10];
+	let compareWrist;
+
+	leftHand.dist = 1000;
+	rightHand.dist = 1000;
+
+	for (let hand of hands) {
+		let wrist = hand.keypoints[0];
+		let selectedHand;
+
+		// for some reason this is inverted
+		if (hand.handedness === "Right") {
+			compareWrist = leftWrist;
+			selectedHand = leftHand;
+		}
+		else {
+			compareWrist = rightWrist;
+			selectedHand = rightHand;
+		}
+
+		let dx = Math.abs(wrist.x - compareWrist.x);
+		let dy = Math.abs(wrist.y - compareWrist.y);
+		let dist = Math.sqrt(dx * dx + dy * dy) / frameSize;
+
+		if (dist < selectedHand.dist) {
+			selectedHand.hand = hand;
+			selectedHand.dist = dist;
+		}
+	}
+
+	if (leftHand.dist == 1000) {
+		leftHand.hand = undefined;
+	}
+	if (rightHand.dist == 1000) {
+		rightHand.hand = undefined;
+	}
+}
+
 function drawHandsKeypoints() {
 	fill(255, 255, 0);
-	for (i = 0; i < hands.length; i++) {
-		let hand = hands[i];
+	let drawHands = [leftHand.hand, rightHand.hand];
+	for (let hand of drawHands) {
+		if (!hand) continue;
 		for (j = 0; j < 21; j++) {
 			let keypoint = hand.keypoints[j];
 			if (keypoint.score < 0.2) continue;
@@ -91,44 +142,39 @@ function drawHandsKeypoints() {
 	}
 }
 
-let leftHand, rightHand;
-
-function pickHands() {
-	if (!poses || poses.length < 1) return;
-
-	let leftWrist = poses[0].keypoints[9];
-	let rightWrist = poses[0].keypoints[10];
-	let compareWrist;
-
-	for (let hand of hands) {
-		let wrist = hand.keypoints[0];
-
-		if (hand.handedness === "Right") compareWrist = leftWrist
-		else compareWrist = rightWrist;
-
-		let dx = Math.abs(wrist.x - compareWrist.x);
-		let dy = Math.abs(wrist.y - compareWrist.y);
-		let dist = Math.sqrt(dx * dx + dy * dy);
-		let distRatio = dist / frameSize;
-
-		console.log(distRatio);
-	}
-}
-
 async function draw() {
 	if (!readyToDraw()) return;
 	await getPoses();
+
+	image(video, 0, 0, width, height);
 
 	// check for hand poses closest to wrists of body pose, to select only relevant hands
 
 	pickHands();
 
 	// infer hand gesture type from 3d hand keypoints (eg. closed or open)
+
+	let gestureLeft = detectGesture(leftHand.hand);
+	let gestureRight = detectGesture(rightHand.hand);
+
+	if (leftHand.hand) {
+		let keypoint = leftHand.hand.keypoints[0];
+		let x = (keypoint.x / 1920) * width;
+		let y = (keypoint.y / 1080) * height;
+		text(gestureLeft, x, y);
+	}
+
+	if (rightHand.hand) {
+		let keypoint = rightHand.hand.keypoints[0];
+		let x = (keypoint.x / 1920) * width;
+		let y = (keypoint.y / 1080) * height;
+		text(gestureRight, x, y);
+	}
+
 	// infer direction of hand (where is tha palm facing? where is the thumb?)
 	// draw a digital twin of two hands using positional data from pose and hand keypoints
 	// use motion, location and rotation of hands to call gesture events (like hold, swipe etc.)
 
-	image(video, 0, 0, width, height);
 	drawPoseKeypoints();
 
 	drawHandsKeypoints();
