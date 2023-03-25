f975c19fff06d4d7416ccc399bb075e0f9389e9a
Frederik Sicking <frederik.sicking@gmail.com>
2023-03-25 22:21:10 +0100

    added actual support for detecting multiple people, picking one and tracking them only

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function setup() {
 	video = createCapture(VIDEO, () => {
 		width = video.width;
 		height = video.height;
-		frameSize = Math.min(width, height);
+		frameSize = Math.max(width, height);
 		createCanvas(width, height);
 		videoDone = true;
 	});
@@ -85,7 +85,10 @@ const fillColors = [
 ]
 
 function drawPoseKeypoints() {
-	for (let pose of poses) {
+	let drawPoses = [person.pose];
+	// let drawPoses = poses;
+	for (let pose of drawPoses) {
+		if (!pose) continue;
 		for (i = 0; i < 17; i++) {
 			fill(fillColors[i]);
 			let keypoint = pose.keypoints[i];
@@ -106,6 +109,51 @@ const wristScoreThreshold = 0.2;
 const maxHandWristDistanceAccurate = 0.2;
 const maxHandWristDistanceFuzzy = 0.4;
 
+const rawScoreMultiplier = 2.0;
+const handednessMultiplier = 1.0;
+const distanceMultiplier = 70.0;
+const distanceDeltaMultiplier = 70.0;
+
+function handFitness(hand) {
+	if (!person.pose) return hand.handedness === "Right" ? hand.score * rawScoreMultiplier : - hand.score * rawScoreMultiplier;
+
+	let wrist = hand.keypoints[0];
+	let leftWrist = person.pose.keypoints[9];
+	let rightWrist = person.pose.keypoints[10];
+
+	let dx = wrist.x - leftWrist.x;
+	let dy = wrist.y - leftWrist.y;
+	let ldist = Math.sqrt(dx * dx + dy * dy) / frameSize;
+
+	dx = wrist.x - rightWrist.x;
+	dy = wrist.y - rightWrist.y;
+	let rdist = Math.sqrt(dx * dx + dy * dy) / frameSize;
+
+	let score = 0;
+
+	// - being left handed with a very high score (actually inverted)
+	if (hand.handedness === "Right") {
+		score += hand.score * handednessMultiplier;
+	} else {
+		score -= hand.score * handednessMultiplier;
+	}
+
+	// - being left handed with a reasonable score and close to the left wrist with a reasonable score
+	// - being close to the left wrist, if it has a very high score
+	score += rdist * distanceMultiplier * rightWrist.score;
+	score -= ldist * distanceMultiplier * leftWrist.score;
+
+	// - being closer to the left than to the right wrist, if both have high scores
+	// - being left handed with a reasonable score and far from the right wrist with a high score
+	score += (rdist - ldist) * distanceDeltaMultiplier * rightWrist.score * leftWrist.score;
+
+	// - being close to the previous left hand or wrist position, if that is recent
+	// TODO: Add this ^^
+
+	return score;
+
+}
+
 function pickHands() {
 	if (!person.pose) return;
 
@@ -121,7 +169,7 @@ function pickHands() {
 			let selectedHand;
 			let maxDist = maxHandWristDistanceAccurate;
 
-			// TODO: The predicted handedness is sometimes wrong!
+			// The predicted handedness is sometimes wrong!
 
 			// What data do i have?
 			// - handedness with score
@@ -137,10 +185,7 @@ function pickHands() {
 			// - being left handed with a reasonable score and far from the right wrist with a high score
 			// - being close to the previous left hand or wrist position, if that is recent
 
-			// TODO: Make a left/right fitness function (eg. positive/negative ?) that makes its calculations 
-			// based on above data and combinations.
-
-			if (hand.handedness === "Right") {
+			if (handFitness(hand) >= 0) {
 				compareWrist = leftWrist;
 				selectedHand = leftHand;
 			}
@@ -180,23 +225,62 @@ function pickHands() {
 	}
 }
 
+const poseScoreMultiplier = 1.0;
+const poseCenterMultiplier = 2.0;
+
+const minFitnessOffset = 0.2;
+const maxPoseDistance = 0.1;
+
+function poseFitness(pose) {
+	let score = 0;
+
+	score += pose.score * poseScoreMultiplier;
+
+	score -= (Math.abs(pose.keypoints[0].x - width / 2) / frameSize) * poseCenterMultiplier;
+
+	return score;
+}
+
 let person = {};
 
 function pickPerson() {
 	if (!poses || poses.length < 1) return;
 
+	let fitnesses = [];
+
+	person.pose = undefined;
+	let fittest = -999999;
 	for (let pose of poses) {
-		// evaluate confidence
-		// get center of pose
-		// store distance to screen space center
-		// choose lowest distance
-		person.pose = pose;
+		let fit = poseFitness(pose);
+		if (fit > fittest) {
+			person.pose = pose;
+			fittest = fit;
+		}
+		fitnesses.push({ pose: pose, fit: fit });
+	}
+
+	if (fitnesses.length >= 2) {
+		fitnesses.sort((a, b) => b.fit - a.fit);
+
+		let a = fitnesses[0].pose;
+		let b = fitnesses[1].pose;
+
+		let dx = a.keypoints[0].x - b.keypoints[0].x;
+		let dy = a.keypoints[0].y - b.keypoints[0].y;
+		let dist = Math.sqrt(dx * dx + dy * dy);
+
+		let dfit = fitnesses[0].fit - fitnesses[1].fit;
+
+		if (dist > maxPoseDistance && dfit < minFitnessOffset) {
+			console.log("multiple people");
+			person.pose = undefined;
+		}
 	}
 }
 
 function drawHandsKeypoints() {
-	// let drawHands = [leftHand.hand, rightHand.hand];
-	let drawHands = hands;
+	let drawHands = [leftHand.hand, rightHand.hand];
+	// let drawHands = hands;
 	for (let hand of drawHands) {
 		if (!hand) continue;
 		fill(255, 255, 0, 64);
